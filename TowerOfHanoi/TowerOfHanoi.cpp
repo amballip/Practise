4dc9a4c2f809773015b960eabbdf7e210ab64901
amballip <amballip@gmail.com>
2020-12-19 19:21:06 +0530

    Code modified

--- TowerOfHanoi/TowerOfHanoi.cpp
+++ TowerOfHanoi/TowerOfHanoi.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
+void towerOfHanoi(int noOfDisks);
 
 int main()
 {
-    cout<<"Enter number of disks";
+    cout<<"Enter number of disks"<<endl;
+    int noOfDisks = 0;
+    cin>>noOfDisks;
     return 0;
+}
+
+void towerOfHanoi(int noOfDisks)
+{
+
 }
